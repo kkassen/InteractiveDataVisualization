2e2f9d65519241440ec7e4a0b84b9ca6850c4777
Kyle Kassen <59017668+kkassen@users.noreply.github.com>
2021-12-27 12:04:34 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,5 @@
-#CSC 468: Group Project
 #Author: Kyle Arick Kassen
-#Group Name: DataVizWhiz
-#Members: Kyle Kassen, Vi Nguyen, Thang Tran, Ximan Liu
-#Reference: Professor Eli T. Brown | CSC 468, Week 7 Live Lecture
+#Reference: Professor Eli T. Brown, DePaul University
 #instructions:
     #1) open terminal under directory
     #2) $set FLASK_APP= server.py
